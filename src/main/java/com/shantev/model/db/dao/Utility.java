1addc92cfe0a90bc50d647beb7fea8cbbd3b3b52
Anton Shapoval <bsnsposhta@gmail.com>
2023-06-09 19:48:56 +0300

    Utility.java class where DAO utility methods is stored

--- src/main/java/com/shantev/model/db/dao/Utility.java
+++ src/main/java/com/shantev/model/db/dao/Utility.java
@@ -1,2 +1,35 @@
-package com.shantev.model.db.dao;public class Utility {
+package com.shantev.model.db.dao;
+
+import com.shantev.exception.DBException;
+import com.shantev.exception.Messages;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class Utility {
+
+    public static Connection getConnection() throws DBException {
+        Connection con;
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/ConferenceDB");
+            con = ds.getConnection();
+        } catch (Exception e) {
+            throw new DBException(Messages.CANNOT_OBTAIN_CONNECTION, e);
+        }
+        return con;
+    }
+
+    public static void close(AutoCloseable resource) {
+        if (resource != null) {
+            try {
+                resource.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
